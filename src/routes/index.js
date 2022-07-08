575085813093d0622b6ec8cf8ccc2a2d2fe39a4b
lyquochoa <lyquochoa000@gmail.com>
2022-07-08 20:50:39 +0700

    update code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import HomeAdmin from "../pages/Home_Admin";
 import AccountManagement from "../pages/Account_Management";
+import Update from "../pages/Update";
 
 // Public routes
 const publicRoutes = [
   { path: "/", component: HomeAdmin },
   { path: "/accountmanagement", component: AccountManagement },
+  { path: "/update", component: Update, layout: null },
 ];
 
 const privateRoutes = [];

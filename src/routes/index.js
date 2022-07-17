e4be96b9b786353f7d6a47aa8f546baa045b2374
lyquochoa <lyquochoa000@gmail.com>
2022-07-17 18:04:10 +0700

    update ui login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,14 @@
 import HomeAdmin from "../pages/Home_Admin";
 import AccountManagement from "../pages/Account_Management";
 import Update from "../pages/Update";
+import Login from "../pages/Login";
 
 // Public routes
 const publicRoutes = [
   { path: "/", component: HomeAdmin },
   { path: "/accountmanagement", component: AccountManagement },
   { path: "/update", component: Update, layout: null },
+  { path: "/login", component: Login, layout: null },
 ];
 
 const privateRoutes = [];
